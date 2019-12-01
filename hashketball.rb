ecc50ebec44c07f5736b56a40dd4d56cfd488e95
Noah Scala <tobi8912@gmail.com>
2019-12-01 22:35:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -34,7 +34,7 @@ def game_hash
         :shoe => 17,
         :points => 17,
         :rebounds => 19, 
-        :assits => 10,
+        :assists => 10,
         :steals => 3,
         :blocks => 1,
         :slam_dunks => 15
