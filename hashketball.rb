992cd1605ede5318d2cd54c517436f0756f1c5f2
Noah Scala <tobi8912@gmail.com>
2019-12-01 22:34:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,7 +182,8 @@ def player_stats(player_name)
       if key == :players 
         value.each do |player|
           if input == player[:player_name]
-            player
+            player.delete(:player_name)
+            return player
           end 
         end 
       end 
