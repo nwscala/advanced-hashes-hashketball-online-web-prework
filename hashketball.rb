ae9f0e27466fa69d01634a9e7a67c87208ec15f5
Noah Scala <tobi8912@gmail.com>
2019-12-01 22:26:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,7 +165,7 @@ def player_numbers(team_name)
   game_hash.each do |team, team_info|
     if team_info[:team_name] == team_name
       team_info.each do |key, value|
-        
+        if key == :players
         numbers.push(player_stats[:number])
       end
     end 
