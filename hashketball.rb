5504a4305821cf7b04521463a5e3ddce6586f0ef
Noah Scala <tobi8912@gmail.com>
2019-12-01 21:05:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,5 +130,5 @@ end
 def num_points_scored(player_search)
   game_hash.each do |team, team_info|
     team_info[:players].each do |player|
-      if 
+      if player[:player_name] == player_search
 end
